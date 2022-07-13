1600beee429a1d832f438910491b5c2bf9ba8f23
bishalr0y <bishalroy895@gmail.com>
2022-07-13 17:07:31 +0530

    heroku setup

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
@@ -20,6 +21,16 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/workouts', workoutRoutes)
+
+// Serve frontend
+// Heroku setup
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname + '../../frontend/build')))
+
+    app.get('*', (req,  res) => res.sendFile(path.resolve(__dirname, '../../', 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 
 // Connect to the DB
