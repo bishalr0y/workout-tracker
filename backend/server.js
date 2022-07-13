56014d5ad8b1804819ecd7c48e64602015be8ef7
bishalr0y <bishalroy895@gmail.com>
2022-07-13 18:39:56 +0530

    host prepare

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,9 @@ app.use('/api/workouts', workoutRoutes)
 // Serve frontend
 // Heroku setup
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname + '../../frontend/build')))
+    app.use(express.static(path.join(__dirname + '/frontend/build')))
 
-    app.get('*', (req,  res) => res.sendFile(path.resolve(__dirname, '../../', 'frontend', 'build', 'index.html')))
+    app.get('*', (req,  res) => res.sendFile(path.resolve(__dirname, './', 'frontend', 'build', 'index.html')))
 } else {
     app.get('/', (req, res) => res.send('Please set to production'))
 }
